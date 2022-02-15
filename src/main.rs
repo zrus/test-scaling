19ef1c64b67691b44ec74386d570060661bc9b69
Tuong Nguyen <tuongn09@gmail.com>
2022-02-15 14:53:17 +0700

    change to use future

--- src/main.rs
+++ src/main.rs
@@ -47,7 +47,7 @@ fn main() {
     for url in cam_list {
         Bastion::children(|children| {
             children.with_exec(|ctx| {
-                blocking! { create_pipeline(url).and_then(|pipeline| main_loop(pipeline, url)) };
+                spawn! { create_pipeline(url).and_then(|pipeline| main_loop(pipeline, url)) }
             })
         });
     }
