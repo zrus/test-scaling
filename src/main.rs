08c659cf2bda8c0ae6e4fb5a58701e913c2b1ff2
Tuong Nguyen <tuongn09@gmail.com>
2022-02-17 18:17:16 +0700

    move env into closure

--- src/main.rs
+++ src/main.rs
@@ -80,7 +80,7 @@ fn main() {
 
     for url in cam_list {
         Bastion::children(move |children| {
-            children.with_exec(|ctx| async move {
+            children.with_exec(move |ctx| async move {
                 let pipeline = match create_pipeline(url) {
                     Ok(pl) => pl,
                     Err(_) => return Err(()),
