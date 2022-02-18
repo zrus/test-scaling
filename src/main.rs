2b85d0ebac579619e43d7b175afc8dd630971270
Tuong Nguyen <tuongn09@gmail.com>
2022-02-18 15:12:15 +0700

    log

--- src/main.rs
+++ src/main.rs
@@ -369,6 +369,15 @@ fn main_loop(pipeline: gst::Pipeline) -> Result<(), Error> {
                 }
                 .into());
             }
+            MessageView::StateChanged(s) => {
+                println!(
+                    "State changed from {:?}: {:?} -> {:?} ({:?})",
+                    s.src().map(|s| s.path_string()),
+                    s.old(),
+                    s.current(),
+                    s.pending()
+                );
+            }
             _ => (),
         }
     }
